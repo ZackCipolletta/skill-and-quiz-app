66937e4fa48a2fd191a5c55258d64f903c573c69
Zachary <ZackCipolletta@gmail.com>
2023-09-12 10:31:59 -0700

    style buttons on splash page, create custom button classes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 
 import './App.css';
+import React from 'react';
+import { Button, AppBar, Toolbar, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Material-UI App</Typography>
+        </Toolbar>
+      </AppBar>
       <div className="App-splash">
         <h1>
           Skill and Quiz  {/* Google web font 'Anton' */}
         </h1>
-        <p>
+        <p style={{ fontWeight: 'bold' }}>
           Your own quiz app
         </p>
-        <div><button>Login</button> <button>SignUp</button></div>
+        <div>
+          <Button className='button-lightBlue'>Login</Button> <span></span>
+          <Button className='button-darkBlue'>SignUp</Button></div>
       </div>
-      
+
     </div>
   );
 }
