2468a8db57cf55c7a111398eb0ae2fd714546127
Zachary <ZackCipolletta@gmail.com>
2023-09-13 11:48:38 -0700

    move AppBar to Control component.

---
+++ src/components/Control.js
@@ -0,0 +1,15 @@
+import './Components.css';
+import React from 'react';
+import { Button, AppBar, Toolbar, Typography } from '@mui/material';
+
+
+<div>
+
+  <AppBar position="static">
+    <Toolbar className='lightBlue-bg'>
+      <Typography className='darkBlue-text' fontWeight='bold' variant="h6">ConnectLink</Typography>
+      <Button className='button-darkBlue'>SignUp</Button>
+    </Toolbar>
+  </AppBar>
+
+</div>
