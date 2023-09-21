b89a99df970a09ebbdc987e39187dcc3f4e04d4d
Zachary <ZackCipolletta@gmail.com>
2023-09-21 09:39:49 -0700

    added styling to typography of QuizCard to prevent category titles from shrinking or moving IconButtons.

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -40,15 +40,17 @@ export default function QuizCard(props) {
               marginTop: '-15px'
             }}
           >
-            <Typography /*gutterBottom variant="h5" component="div" */>
-              {props.category}
+            <Typography /*gutterBottom variant="h5" component="div" */ sx={{ flex: 1, minWidth: 0 }}> 
+            {/* By default, flex containers will try to distribute space among their children elements based 
+            on their content and available space. When you add content to one child element, it can affect the layout of other child elements. */}
+              {/* {props.category} */}
             </Typography>
-            <div style={{ marginLeft: '80px' }}>
+            <div style={{ marginLeft: '120px' }}>
               {/* scale enlarges the icons in both X and Y directions, ScaleY stretches left right  */}
-              <IconButton sx={{ width: "50%", marginLeft: '5px', transform: "rotate(90deg) scale(1.1) scaleY(1.2)" }} >
+              <IconButton sx={{ width: "50%", marginLeft: '5px', marginRight: '-5px', transform: "rotate(90deg) scale(.8) scaleY(1.2)" }} >
                 <PiTagChevronThin color='black' />
               </IconButton>
-              <IconButton sx={{ width: "50%", marginLeft: '-5px', transform: "scale(1.1) scaleY(1.2)" }}>
+              <IconButton sx={{ width: "50%", marginLeft: '-5px', transform: "scale(.8) scaleY(1.2)" }}>
                 <PiTrashThin color='red' />
               </IconButton>
             </div>
