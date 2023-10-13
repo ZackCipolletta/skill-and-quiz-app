7b4a31b7139130c0a579db21ab82936c13585864
Zachary <ZackCipolletta@gmail.com>
2023-10-13 13:20:14 -0700

    add schedule tab with start and end calendar and times

--- src/SignIn-SignUp/test2.js
+++ src/SignIn-SignUp/test2.js
@@ -1,23 +1,139 @@
-import * as React from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+<Box>
 
-export default function DatePickerValue() {
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+<Box sx={{ marginBottom: 5 }}>
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <Typography>Start Date</Typography>
+    <DatePicker
+      value={startDate}
+      onChange={(newStartDate) => setStartDate(newStartDate)}
+      sx={{
+        '& .MuiOutlinedInput-root': {
+          borderRadius: 2, // This will make the border radius 10px
+        },
+        '& .MuiOutlinedInput-input': {
+          padding: '5px', // Adjust the padding as needed
+        }
+      }}
+    />
+
+  </LocalizationProvider >
+</Box>
+
+<Box>
+  <LocalizationProvider>
+    <Typography>End Date</Typography>
+    <DatePicker
+      value={startDate}
+      onChange={(newStartDate) => setStartDate(newStartDate)}
+      sx={{
+        '& .MuiOutlinedInput-root': {
+          borderRadius: 2, // This will make the border radius 10px
+        },
+        '& .MuiOutlinedInput-input': {
+          padding: '5px', // Adjust the padding as needed
+        }
+      }}
+    />
+
+  </LocalizationProvider >
+</Box>
+
+</Box>;
+
+
+
+
+
+
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import {
+  Box, Button, Paper, Tabs, Tab, Typography, InputLabel,
+  TextField, FormControl, Select, MenuItem, IconButton,
+} from '@mui/material';
+import ColorTemplates from './ColorTemplates';
+import '../Styles/Components.css';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import { FaFileCsv } from 'react-icons/fa';
+import { TfiClose } from 'react-icons/tfi';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import dayjs from 'dayjs';
+
+
+import dayjs, { Dayjs } from 'dayjs';
+
+export default function QuizSchedule() {
+
+  const [startDate, setStartDate] = React.useState(dayjs('2022-04-17'));
+  // const [endDate, setEndDate] = React.useState < Dayjs | null > (dayjs('2022-04-17'));
+
 
   return (
+
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker', 'DatePicker']}>
-        <DatePicker label="Uncontrolled picker" defaultValue={dayjs('2022-04-17')} />
+
+      <Box sx={{mb: 2}}>
+        <Typography>Start Date</Typography>
         <DatePicker
-          label="Controlled picker"
-          value={value}
-          onChange={(newValue) => setValue(newValue)}
+          value={startDate}
+          onChange={(newStartDate) => setStartDate(newStartDate)}
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              borderRadius: 2, // This will make the border radius 10px
+            },
+            '& .MuiOutlinedInput-input': {
+              padding: '5px', // Adjust the padding as needed
+            }
+          }}
         />
-      </DemoContainer>
-    </LocalizationProvider>
+      </Box>
+
+      <Box>
+        <Typography>End Date</Typography>
+        <DatePicker
+          value={startDate}
+          onChange={(newStartDate) => setStartDate(newStartDate)}
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              borderRadius: 2, // This will make the border radius 10px
+            },
+            '& .MuiOutlinedInput-input': {
+              padding: '5px', // Adjust the padding as needed
+            }
+          }}
+        />
+      </Box>
+    </LocalizationProvider >
+
+    // <LocalizationProvider
+    //   dateAdapter={AdapterDayjs}
+    //   sx={{ width: '100%' }}>
+
+    //   <Typography>Start Date</Typography>
+    //   <DatePicker
+    //       label="Controlled picker"
+    //       // value={startDate}
+    //       // onChange={(newStartDate) => setStartDate(newStartDate)}
+    //     />
+
+
+    //   <Typography>Start Time</Typography>
+
+
+    //   <Typography>End Date</Typography>
+    //   <DatePicker
+    //       label="Controlled picker"
+    //       // value={endDate}
+    //       // onChange={(newEndDate) => setEndDate(newEndDate)}
+    //     />
+
+
+    //   <Typography>End Time</Typography>
+
+
+    // </LocalizationProvider>
   );
-}
+};  
--- src/components/QuizSchedule.jsx
+++ src/components/QuizSchedule.jsx
@@ -11,7 +11,7 @@ import { FaFileCsv } from 'react-icons/fa';
 import { TfiClose } from 'react-icons/tfi';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider, DatePicker, TimePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 // import dayjs from 'dayjs';
 
@@ -21,50 +21,85 @@ import dayjs, { Dayjs } from 'dayjs';
 export default function QuizSchedule() {
 
   const [startDate, setStartDate] = React.useState(dayjs('2022-04-17'));
+  const [startTime, setStartTime] = React.useState(dayjs('2022-04-17T15:30'));
+  const [endDate, setEndDate] = React.useState(dayjs('2022-04-17'));
+  const [endTime, setEndTime] = React.useState(dayjs('2022-04-17T15:30'));
   // const [endDate, setEndDate] = React.useState < Dayjs | null > (dayjs('2022-04-17'));
 
 
   return (
+
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        
-        label="Controlled picker"
-        value={startDate}
-        onChange={(newStartDate) => setStartDate(newStartDate)}
-        sx={{
-          '& .MuiOutlinedInput-root': {
-            borderRadius: 2, // This will make the border radius 10px
-          },
-        }}
-      />
-    </LocalizationProvider>
 
-    // <LocalizationProvider
-    //   dateAdapter={AdapterDayjs}
-    //   sx={{ width: '100%' }}>
+      <Box sx={{ mb: 5, display: 'flex' }}>
+        <Box sx={{ mr: 5 }}>
+          <Typography>Start Date</Typography>
+          <DatePicker
+            value={startDate}
+            onChange={(newStartDate) => setStartDate(newStartDate)}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2, // This will make the border radius 10px
+              },
+              '& .MuiOutlinedInput-input': {
+                padding: '5px', // Adjust the padding as needed
+              }
+            }}
+          />
+        </Box>
 
-    //   <Typography>Start Date</Typography>
-    //   <DatePicker
-    //       label="Controlled picker"
-    //       // value={startDate}
-    //       // onChange={(newStartDate) => setStartDate(newStartDate)}
-    //     />
+        <Box>
+          <Typography>Start Time</Typography>
+          <TimePicker
+            value={startTime}
+            onChange={(newStartTime) => setStartTime(newStartTime)}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2, // This will make the border radius 10px
+              },
+              '& .MuiOutlinedInput-input': {
+                padding: '5px', // Adjust the padding as needed
+              }
+            }}
+          />
+        </Box>
+      </Box>
 
+      <Box sx={{ mb: 2, display: 'flex' }}>
+        <Box sx={{ mr: 5 }}>
+          <Typography>End Date</Typography>
+          <DatePicker
+            value={endDate}
+            onChange={(newEndDate) => setEndDate(newEndDate)}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2, // This will make the border radius 10px
+              },
+              '& .MuiOutlinedInput-input': {
+                padding: '5px', // Adjust the padding as needed
+              }
+            }}
+          />
+        </Box>
 
-    //   <Typography>Start Time</Typography>
+        <Box>
+          <Typography>End Time</Typography>
+          <TimePicker
+            value={endTime}
+            onChange={(newEndTime) => setEndTime(newEndTime)}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2, // This will make the border radius 10px
+              },
+              '& .MuiOutlinedInput-input': {
+                padding: '5px', // Adjust the padding as needed
+              }
+            }}
+          />
+        </Box>
+      </Box>
 
+    </LocalizationProvider >
 
-    //   <Typography>End Date</Typography>
-    //   <DatePicker
-    //       label="Controlled picker"
-    //       // value={endDate}
-    //       // onChange={(newEndDate) => setEndDate(newEndDate)}
-    //     />
-
-
-    //   <Typography>End Time</Typography>
-
-
-    // </LocalizationProvider>
   );
 };  
