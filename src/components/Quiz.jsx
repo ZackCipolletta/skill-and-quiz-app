496cffdf7ceed7174ed9cb8b8371ecb6ffdcc485
Zachary <ZackCipolletta@gmail.com>
2023-10-17 16:50:07 -0700

    WIP building quiz info page, adding questions and answers table

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,10 @@
 
 import '../Styles/Components.css';
 import React, { useState, useEffect } from "react";
-import { Button, Box, IconButton } from '@mui/material';
+import {
+  Button, Box, IconButton, TableContainer, Table, TableHead,
+  TableRow, TableCell, TableBody, Paper
+} from '@mui/material';
 import SearchBar from './SearchBar';
 import { useNavigate } from 'react-router-dom';
 import DeleteModal from './DeleteModal';
@@ -16,63 +19,79 @@ export default function Quiz() {
   const navigate = useNavigate();
 
   return (
-    <Box style={{ marginTop: 35 }}>
-      <Box style={{
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: 5
-      }}>
-        <Box style={{
-          backgroundColor: quizInfo.Color,
-          borderRadius: 50,
-          width: 65,
-          height: 65,
-          marginRight: 5
-        }}
-        >
+    <Box
+      style={{ marginTop: 35 }}>
+      
+      <Box name='name&Buttons'
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: 5
+        }}> 
+        
+        <Box name='quizImage&Name'
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginBottom: 5
+          }}>
+          
+          <Box name='quizName'
+            style={{
+              backgroundColor: quizInfo.Color,
+              borderRadius: 50,
+              width: 65,
+              height: 65,
+              marginRight: 5
+            }}
+          />
+
+          <h1 className='darkBlue-text'>
+            {quizInfo.Name}
+          </h1>
+        </Box> {/* quizImage&Name closes */}
+
+        <Box name='buttons'>
+          <IconButton className='button-lightGray'
+            style={{
+              borderRadius: 5,
+              mr: 5,
+              marginRight: 1,
+              paddingBottom: 0,
+              paddingTop: 0,
+              paddingLeft: 3,
+              paddingRight: 3,
+              transform: "rotate(90deg) scale(.8) scaleY(1.2)"
+            }}>
+            <PiTagChevron />
+          </IconButton>
+
+          <IconButton className='button-lightGreen'
+            style={{
+              borderRadius: 5,
+              padding: 3,
+              // mr: 1,
+              transform: "scale(.8)"
+            }}>
+            <LuAward />
+          </IconButton>
+
+          <Button
+            className='button-redButton'
+            sx={{ ml: 1, mr: 1, padding: 0 }}>
+            Delete
+          </Button>
+          <Button
+            className='button-mediumBlue'
+            sx={{ padding: 0 }}
+          >
+            Cancel
+          </Button>
         </Box>
-        <h1 className='darkBlue-text'>
-          {quizInfo.Name}
-        </h1>
-      </Box>
-      <Box>
-        <IconButton className='button-lightGray'
-          style={{
-            borderRadius: 5,
-            mr: 5,
-            marginRight: 1,
-            paddingBottom: 0,
-            paddingTop: 0,
-            paddingLeft: 3,
-            paddingRight: 3,
-            transform: "rotate(90deg) scale(.8) scaleY(1.2)"
-          }}>
-          <PiTagChevron />
-        </IconButton>
+      </Box> {/* name&Buttons closes */}
 
-        <IconButton className='button-lightGreen'
-          style={{
-            borderRadius: 5,
-            padding: 3,
-            // mr: 1,
-            transform: "scale(.8)"
-          }}>
-          <LuAward />
-        </IconButton>
-
-        <Button
-          className='button-redButton'
-          sx={{ ml: 1, mr: 1, padding: 0 }}>
-          Delete
-        </Button>
-        <Button
-          className='button-mediumBlue'
-          sx={{ padding: 0 }}
-        >
-          Cancel
-        </Button>
-      </Box>
-      <Box>
+      <Box name='tags'>
         {quizInfo.tags.map((tag, index) => (
           <span
             key={index}
@@ -91,6 +110,36 @@ export default function Quiz() {
           </span>
         ))}
       </Box>
+
+      <TableContainer component={Paper}>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell><h1>#</h1></TableCell>
+            <TableCell><h1>Questions</h1></TableCell>
+            <TableCell><h1>3</h1></TableCell>
+            <TableCell><h1>4</h1></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* Add your rows and cells here */}
+          <TableRow>
+            <TableCell>1</TableCell>
+              <TableCell>{quizInfo.question}</TableCell>
+            <TableCell>3</TableCell>
+            <TableCell>4</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>2</TableCell>
+            <TableCell>Where are you from?</TableCell>
+            <TableCell>Cell 3 Content</TableCell>
+            <TableCell>Cell 4 Content</TableCell>
+          </TableRow>
+          {/* Add more rows as needed */}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
     </Box>
   );
 }
