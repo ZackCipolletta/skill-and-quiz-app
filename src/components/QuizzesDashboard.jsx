942f185d603b4110ef00f2edad4d58fbc2d85f9a
Zachary <ZackCipolletta@gmail.com>
2023-12-07 16:56:49 -0800

    quizzes are correctly filtered by category name in redux.

--- src/components/QuizzesDashboard.jsx
+++ src/components/QuizzesDashboard.jsx
@@ -22,7 +22,7 @@ import {
 
 
 export default function QuizzesDashboard() {
-  const colletionRef = collection(db, 'quizzes');
+  const collectionRef = collection(db, 'quizzes');
 
   const dispatch = useDispatch();
 
@@ -49,121 +49,127 @@ export default function QuizzesDashboard() {
 
 
   useEffect(() => {
-    const unsub = onSnapshot(colletionRef, (querySnapshot) => {
+    // Set up a snapshot listener for the 'quizzes' collection in Firebase
+    const querySnapshotHandler = (querySnapshot) => {
       const quizzes = [];
       querySnapshot.forEach((doc) => {
         quizzes.push(doc.data());
       });
-      dispatch(addQuiz(quizzes));
-    });
-    return () => {
-      unsub();
+      dispatch(setQuizzesArray(quizzes));
     };
-  }, []);
 
+  // Subscribe to the snapshot listener
+  const unsub = onSnapshot(collectionRef, querySnapshotHandler);
 
-
-
-
-
-
-  const handleCreateNewQuizClick = () => {
-    dispatch(setQuizCategory(catName));
-    navigate('/newquiz');
+  // Clean up the listener when the component unmounts
+  return () => {
+    unsub();
   };
+}, []);
 
 
-  const handleSearch = (searchValue) => {
-    // in order to search the complete list each time the search query is modified (ex: French > F should search the entire list containing 'f', not just the list containing 'french'), because the list is modified and then returned to us upon each search, we must reset the list. 
-    dispatch(resetQuizzes());
 
-    // we have created a reducer which searches through the list of quizzes
-    dispatch(searchQuizzes(searchValue));
-  };
 
-  const handleDeleteButtonClick = (event, id, quiz) => {
-    seDeleteModalState(!deleteModalState);
-    setSelectedQuiz(quiz);
-    setIdOfQuizToDelete(id);
-  };
 
-  const reset = () => {
-    setIdOfQuizToDelete([]);
-    setSelectedQuiz([]);
-  };
 
-  const handleDeleteConfirm = () => {
-    seDeleteModalState(!deleteModalState);
-    dispatch(deleteQuiz(idOfQuizToDelete));
 
-    reset();
-  };
+const handleCreateNewQuizClick = () => {
+  dispatch(setQuizCategory(catName));
+  navigate('/newquiz');
+};
 
-  const handleFavoriteButtonClick = (id) => {
-    console.log("Fav icon clicked. Id value is: " + id);
-    const index = quizzesArray.findIndex((quiz) => quiz.id === id);
 
-    if (index !== -1) {
-      const updatedQuizzesArray = [...quizzesArray];
-      updatedQuizzesArray[index].Favorite = !updatedQuizzesArray[index].Favorite;
+const handleSearch = (searchValue) => {
+  // in order to search the complete list each time the search query is modified (ex: French > F should search the entire list containing 'f', not just the list containing 'french'), because the list is modified and then returned to us upon each search, we must reset the list. 
+  dispatch(resetQuizzes());
 
-      setQuizzesArray(updatedQuizzesArray);
-    }
+  // we have created a reducer which searches through the list of quizzes
+  dispatch(searchQuizzes(searchValue));
+};
 
-    reset();
-  };
+const handleDeleteButtonClick = (event, id, quiz) => {
+  seDeleteModalState(!deleteModalState);
+  setSelectedQuiz(quiz);
+  setIdOfQuizToDelete(id);
+};
 
-  const handleNoSearchValue = () => {
-    dispatch(resetQuizzes());
-  };
+const reset = () => {
+  setIdOfQuizToDelete([]);
+  setSelectedQuiz([]);
+};
 
-  return (
-    <Box style={!isMobile ? { marginTop: 20 } : { marginTop: 1 }} >
-      <Box name="TopNav" style={{
+const handleDeleteConfirm = () => {
+  seDeleteModalState(!deleteModalState);
+  dispatch(deleteQuiz(idOfQuizToDelete));
+
+  reset();
+};
+
+const handleFavoriteButtonClick = (id) => {
+  console.log("Fav icon clicked. Id value is: " + id);
+  const index = quizzesArray.findIndex((quiz) => quiz.id === id);
+
+  if (index !== -1) {
+    const updatedQuizzesArray = [...quizzesArray];
+    updatedQuizzesArray[index].Favorite = !updatedQuizzesArray[index].Favorite;
+
+    setQuizzesArray(updatedQuizzesArray);
+  }
+
+  reset();
+};
+
+const handleNoSearchValue = () => {
+  dispatch(resetQuizzes());
+};
+
+return (
+  <Box style={!isMobile ? { marginTop: 20 } : { marginTop: 1 }} >
+    <Box name="TopNav" style={{
+      display: !isMobile ? 'flex' : 'block',
+      justifyContent: !isMobile ? 'space-between' : 'flex-start',
+      alignItems: 'center'
+    }}
+    >
+      <h1 className='darkBlue-text'>
+        Quizzes  {/* Google web font 'Anton' */}
+      </h1>
+
+      <Box name="Search&Button" style={{
         display: !isMobile ? 'flex' : 'block',
-        justifyContent: !isMobile ? 'space-between' : 'flex-start',
         alignItems: 'center'
-      }}
-      >
-        <h1 className='darkBlue-text'>
-          Quizzes  {/* Google web font 'Anton' */}
-        </h1>
+      }}>
+        <SearchBar
+          value=""
+          onChange={handleSearch}
+          onSearch={handleSearch}
+          placeholder={"Search quizzes..."}
+          options={quizzesArray.map((quiz) => quiz.Name)}
+          handleNoSearchValue={handleNoSearchValue}
+        />
 
-        <Box name="Search&Button" style={{
-          display: !isMobile ? 'flex' : 'block',
-          alignItems: 'center'
-        }}>
-          <SearchBar
-            value=""
-            onChange={handleSearch}
-            onSearch={handleSearch}
-            placeholder={"Search quizzes..."}
-            options={quizzesArray.map((quiz) => quiz.Name)}
-            handleNoSearchValue={handleNoSearchValue}
-          />
+        {/* 'ml' does not work here, we must use marginLeft */}
+        <Button
+          className='navButton button-mediumBlue'
+          style={!isMobile ? { marginLeft: '50px' } : { marginTop: 20 }}
+          onClick={(event) => handleCreateNewQuizClick()}
+        >
+          <AddIcon />
+          Create new quiz
+        </Button>
+      </Box> {/* Search&Button closes */}
 
-          {/* 'ml' does not work here, we must use marginLeft */}
-          <Button
-            className='navButton button-mediumBlue'
-            style={!isMobile ? { marginLeft: '50px' } : { marginTop: 20 }}
-            onClick={(event) => handleCreateNewQuizClick()}
-          >
-            <AddIcon />
-            Create new quiz
-          </Button>
-        </Box> {/* Search&Button closes */}
-
-      </Box> {/* TopNav closes */}
-      <Quizzes
-        deleteClick={handleDeleteButtonClick}
-        favorite={handleFavoriteButtonClick}
-      />
-      <DeleteModal
-        toggle={deleteModalState}
-        handleClose={handleDeleteButtonClick}
-        selectedCard={`"${selectedQuiz}" quiz`}
-        handleDeleteConfirm={handleDeleteConfirm}
-      />
-    </Box>
-  );
+    </Box> {/* TopNav closes */}
+    <Quizzes
+      deleteClick={handleDeleteButtonClick}
+      favorite={handleFavoriteButtonClick}
+    />
+    <DeleteModal
+      toggle={deleteModalState}
+      handleClose={handleDeleteButtonClick}
+      selectedCard={`"${selectedQuiz}" quiz`}
+      handleDeleteConfirm={handleDeleteConfirm}
+    />
+  </Box>
+);
 }
